ad13f6a60eab4abaa06cdf65a3333aaf958c9802
Ujjawal Shrivastava <ujjawalshrivastava19@gmail.com>
2021-08-02 01:11:15 +0530

    Changed Home Style

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class MyApp extends StatelessWidget {
           }
         },
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.green,
         ),
       ),
     );
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,37 +12,52 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          TextField(
-            controller: GameClient.of(context)?.playerName,
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              var client = GameClient.of(context)!.artemisClient;
-              var createLobby = CreateLobbyMutation(
-                variables: CreateLobbyArguments(
-                  playerId: GameClient.of(context)!.playerId,
-                  playerName: GameClient.of(context)!.playerName.text,
-                ),
-              );
-              var result = await client.execute(createLobby);
-              print("Created Room Id ${result.data?.createLobby}");
-              var roomId = result.data?.createLobby;
-              if (roomId != null) {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => GameMessageBuilder(
-                      roomId: roomId,
+      body: Center(
+        child: Container(
+          constraints: BoxConstraints(minWidth: 300, maxWidth: 600),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                  controller: GameClient.of(context)?.playerName,
+                  decoration: InputDecoration(
+                    hintText: 'Player Name',
+                    border: OutlineInputBorder(),
+                  )),
+              SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    textStyle:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    padding:
+                        EdgeInsets.symmetric(vertical: 20, horizontal: 40)),
+                onPressed: () async {
+                  var client = GameClient.of(context)!.artemisClient;
+                  var createLobby = CreateLobbyMutation(
+                    variables: CreateLobbyArguments(
                       playerId: GameClient.of(context)!.playerId,
+                      playerName: GameClient.of(context)!.playerName.text,
                     ),
-                  ),
-                );
-              }
-            },
-            child: Text("Create Lobby"),
-          )
-        ],
+                  );
+                  var result = await client.execute(createLobby);
+                  print("Created Room Id ${result.data?.createLobby}");
+                  var roomId = result.data?.createLobby;
+                  if (roomId != null) {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => GameMessageBuilder(
+                          roomId: roomId,
+                          playerId: GameClient.of(context)!.playerId,
+                        ),
+                      ),
+                    );
+                  }
+                },
+                child: Text("Create Lobby"),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
