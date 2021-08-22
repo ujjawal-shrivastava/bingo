cd47c4d4a0639f6b42cfd2b94f50f9f51fe67037
Deep Gaurav <deepgauravraj@gmail.com>
2021-08-22 09:48:11 +0530

    use canvaskit

--- build_vercel.sh
+++ build_vercel.sh
@@ -1,2 +1,2 @@
 git clone -b stable https://github.com/flutter/flutter.git
-./flutter/bin/flutter build web --dart-define=BACKEND_URL=$BACKEND_URL
+./flutter/bin/flutter build web --dart-define=BACKEND_URL=$BACKEND_URL --web-renderer canvaskit
